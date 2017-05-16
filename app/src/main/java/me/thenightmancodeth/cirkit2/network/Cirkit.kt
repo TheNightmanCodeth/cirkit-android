4a796eec5c08e64a1e958a280a42515f617f9caf
TheNightman <joyod3@gmail.com>
2017-05-16 17:01:49 -0400

    Fixes build and initial push impl

--- app/src/main/java/me/thenightmancodeth/cirkit2/network/Cirkit.kt
+++ app/src/main/java/me/thenightmancodeth/cirkit2/network/Cirkit.kt
@@ -1,5 +1,6 @@
 package me.thenightmancodeth.cirkit2.network
 
+import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.httpPost
 
@@ -8,11 +9,11 @@ import com.github.kittinunf.fuel.httpPost
  */
 class Cirkit {
     init {
-        FuelManager.instance.basePath = "http://10.0.0.245:5500/"
+        FuelManager.instance.baseHeaders = mapOf("Content-Type" to "application/json")
     }
 
     fun sendStringPush(msg: String) {
-        "/msg".httpPost().body("{ \"msg\": \"$msg\"}").responseString {request, response, result ->
+        Fuel.post("http://10.0.0.245:5500/msg").body("{ \"msg\" : \"$msg\" }").response { request, response, result ->
             println(response)
             val (bytes, error) = result
             if (bytes != null) {
